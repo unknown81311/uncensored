e9897393aab40ce0a8669154517f0d5a1325973e
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-06-21 21:33:16 -0400

    Add CI/CD deployment config and release config

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# fr.to
+# Uncensored.
+
+An Unknown81311 discord bot. Ask him what it does, I don't know.
+Send <a href="https://dotglitch.dev">DotGlitch</a> a cookie.
--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@ git config user.name "GoCD Automation"
 git config user.email "no-reply@dotglitch.dev"
 
 npm version patch
-version=$(npm version --json | jq '."fr.to"' | tr -d '"')
+version=$(npm version --json | jq '."uncensored"' | tr -d '"')
 
 # Run the build
 npm i
 npm run build
-docker build . -t harbor.dotglitch.dev/library/fr-to:$version
+docker build . -t harbor.dotglitch.dev/library/cyle-uncensored:$version
 
 # Once built, push the new build number
 git add package.json
@@ -16,8 +16,7 @@ git commit -m "Bump version"
 git push
 
 # Push the new docker image
-docker push harbor.dotglitch.dev/library/fr-to:$version
+docker push harbor.dotglitch.dev/library/cyle-uncensored:$version
 
 # Inject the version number into the deployment files
 sed -i -e "s/:latest/:$version/g" k3s.yml
-sed -i -e "s/:latest/:$version/g" k3s.prod.yml
